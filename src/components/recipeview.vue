c17bbf2d539a95ee67e66ee45bc59556a6c9de7a
soph-dev <sophia.ochs46@gmail.com>
2021-01-13 15:37:59 +0100

    latest changes

--- src/components/recipeview.vue
+++ src/components/recipeview.vue
@@ -43,9 +43,7 @@ export default {
     beforeMount() {
     recipeByIdHelper();
   },
-  props: [
-    
-  ]
+ 
 
 
 };
--- src/helpers/recipeById.js
+++ src/helpers/recipeById.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 export var mealInfoById; 
+export var mealName;
 export function recipeByIdHelper() {
     let mealId = "52772";
     let idUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId;
-
 
     axios.get(idUrl)
         .then(function (response) {
