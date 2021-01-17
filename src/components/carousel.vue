8bea0f12d12beabc1942f2cce7be5a069afbf401
Fi0x <felpech@aol.com>
2021-01-17 11:31:12 +0100

    Enabled carousel to add new random recipes to the front

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -6,38 +6,33 @@
       <!--Both Chevrons are Scaling Vector Graphics from https://kruxor.com/view/code/1pheD/chevron-left/-->
       <div>
         <svg
-          width="3em"
-          height="3em"
-          viewBox="0 0 16 16"
-          class="chevron"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          v-on:click="previousRecipe"
+            width="3em"
+            height="3em"
+            viewBox="0 0 16 16"
+            class="chevron"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            v-on:click="previousRecipe"
         >
           <path
-            fill-rule="evenodd"
-            d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
           />
         </svg>
-        <img
-          v-bind:src="mealImg"
-          v-bind:alt="mealName"
-          id="carouselimg"
-          v-on:click="recipeClicked"
-        />
+        <img v-bind:src="mealImg" v-bind:alt="mealName" id="carouselimg" v-on:click="recipeClicked"/>
         <!--Both Chevrons are Scaling Vector Graphics from https://kruxor.com/view/code/1pheD/chevron-right/-->
         <svg
-          width="3em"
-          height="3em"
-          viewBox="0 0 16 16"
-          class="chevron"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          v-on:click="nextRecipe"
+            width="3em"
+            height="3em"
+            viewBox="0 0 16 16"
+            class="chevron"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            v-on:click="nextRecipe"
         >
           <path
-            fill-rule="evenodd"
-            d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
           />
         </svg>
       </div>
@@ -53,6 +48,7 @@ export default {
     return {
       index: 0,
       mealIDs: [],
+      currentID: "",
       mealName: "",
       mealImg: "",
       mealInstructions: "",
@@ -61,8 +57,7 @@ export default {
     };
   },
   beforeMount() {
-    this.randomrecipe();
-    //state = landingPage
+    this.firstRecipe();
   },
   methods: {
     recipeClicked() {
@@ -70,23 +65,26 @@ export default {
     },
     async previousRecipe() {
       this.index--;
-      if (this.index < 0) this.index = this.mealIDs.length - 1;
-      await this.recipeByID(this.mealIDs[this.index]);
+      if (this.index < 0) {
+        this.index = 0;
+        await this.randomrecipe();
+        this.mealIDs.unshift(this.currentID);
+      } else await this.recipeByID(this.mealIDs[this.index]);
     },
     async nextRecipe() {
       this.index++;
-      if (this.index < this.mealIDs.length)
-        await this.recipeByID(this.mealIDs[this.index]);
-      else await this.randomrecipe();
+      if (this.index >= this.mealIDs.length) {
+        await this.randomrecipe();
+        this.mealIDs.push(this.currentID);
+      } else await this.recipeByID(this.mealIDs[this.index]);
     },
     async recipeByID(id) {
       let apiUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id;
       let mealInfo;
 
       try {
-        this.response = await this.axios
-          .get(apiUrl) //promise should await this response
-          .then((response) => (mealInfo = response.data.meals[0]));
+        await this.axios.get(apiUrl) //promise should await this response
+            .then((response) => (mealInfo = response.data.meals[0]));
         this.mealName = mealInfo.strMeal;
         this.mealImg = mealInfo.strMealThumb;
         this.mealInstructions = mealInfo.strInstructions;
@@ -106,13 +104,31 @@ export default {
           .then((response) => (mealInfo = response.data.meals[0]));
         if (this.mealIDs.includes(mealInfo.idMeal)) await this.randomrecipe();
         else {
-          this.mealIDs.push(mealInfo.idMeal);
+          this.currentID = mealInfo.idMeal;
           this.mealName = mealInfo.strMeal;
           this.mealImg = mealInfo.strMealThumb;
           this.mealInstructions = mealInfo.strInstructions;
           this.mealCategory = mealInfo.strCategory;
           this.mealArea = mealInfo.strArea;
         }
+      } catch (e) {
+        console.error(e); //throws error if promise can't be fulfilled
+      }
+    },
+    async firstRecipe() {
+      let apiUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+      let mealInfo;
+
+      try {
+        await this.axios.get(apiUrl) //promise should await this response
+            .then((response) => (mealInfo = response.data.meals[0]));
+        this.mealIDs.push(mealInfo.idMeal);
+        this.mealName = mealInfo.strMeal;
+        this.mealImg = mealInfo.strMealThumb;
+        this.mealInstructions = mealInfo.strInstructions;
+        this.mealCategory = mealInfo.strCategory;
+        this.mealArea = mealInfo.strArea;
+
       } catch (e) {
         console.error(e); //throws error if promise can't be fulfilled
       }
