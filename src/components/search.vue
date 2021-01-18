8973fd8961d2888276a721c27b01a033140e9d6d
soph-dev <sophia.ochs46@gmail.com>
2021-01-18 16:11:35 +0100

    started to work on search layout

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,27 +1,46 @@
 <!--------------------------------TEMPLATE----------------------------------->
 <template>
   <div>
-    <!-- todo dummy searchbar for development. 
-Should be exported to header.vue later.-->
-    <!--todo error catch user inputs-->
+    <!--TODO: error catch user inputs
+    TODO: make search query work in both searchbars? /do we even need the searchbar 
+    over the search results?-->
     <div id="search">
       <b-form-input
-          size="sm"
-          class="mr-sm-2"
-          placeholder="Search"
-          v-model="userInput"
+        size="sm"
+        class="mr-sm-2"
+        placeholder="Looking for something else instead?"
+        v-model="userInput"
       ></b-form-input>
     </div>
     <div id="searchresults">
-      You searched for "{{ userInput }}" These are your results:
+      <!--TODO: filter key object so only the search query gets displayed here-->
+      You searched for <b>{{ key }}</b
+      >. These are your results:
       <ul>
         <li v-for="(result, idx) in mealInfo" :key="idx">
           <router-link :to="`/recipe=${result.idMeal}`" class="router-links">
-            <img v-bind:src="result.strMealThumb" v-bind:alt="result.strMeal"
-                 v-on:click="recipeClicked(result.idMeal)"/>
-            <h5 v-on:click="recipeClicked(result.idMeal)">{{ result.strMeal }}</h5>
+            <b-card
+              v-bind:img-src="result.strMealThumb"
+              v-bind:img-alt="result.strMeal"
+              v-on:click="recipeClicked(result.idMeal)"
+              img-left
+              class="mb-3"
+            >
+              <b-card-text>
+                Some quick example text to build on the card and make up the
+                bulk of the card's content.
+              </b-card-text>
+            </b-card>
+            <h5 v-on:click="recipeClicked(result.idMeal)">
+              {{ result.strMeal }}
+            </h5>
+
+            <div id="tags">{{ result.strCategory }} {{ result.strArea }}</div>
           </router-link>
-          <div id="tags">{{ result.strCategory }} {{ result.strArea }}</div><!--On click start search for category-->
+
+          <!--TODO: show first ?20? words of instructions next to each meal as a teaser-->
+
+          <!--On click start search for category-->
         </li>
       </ul>
     </div>
@@ -38,19 +57,22 @@ export default {
       mealInfo: "",
     };
   },
-  props: ['key'],
+  props: ["key"],
   beforeMount() {
     this.recipeSearchKey();
   },
 
   methods: {
     recipeClicked(recipeID) {
-      this.$root.$emit('displayRecipe', recipeID)
+      this.$root.$emit("displayRecipe", recipeID);
     },
     async recipeSearchKey() {
-      let apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?" + this.key;
+      let apiUrl =
+        "https://www.themealdb.com/api/json/v1/1/search.php?" + this.key;
       try {
-        await this.axios.get(apiUrl).then((response) => (this.mealInfo = response.data.meals));
+        await this.axios
+          .get(apiUrl)
+          .then((response) => (this.mealInfo = response.data.meals));
       } catch (e) {
         console.error(e);
       }
@@ -58,11 +80,13 @@ export default {
     async recipeSearchIngredient() {
       //load function asynchronically
       let response;
-      let apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + this.userInput;
+      let apiUrl =
+        "https://www.themealdb.com/api/json/v1/1/filter.php?i=" +
+        this.userInput;
       try {
         response = await this.axios
-            .get(apiUrl) //promise should await this response
-            .then((response) => (this.mealInfo = response.data.meals));
+          .get(apiUrl) //promise should await this response
+          .then((response) => (this.mealInfo = response.data.meals));
         console.log(response);
       } catch (e) {
         console.error(e);
@@ -71,11 +95,13 @@ export default {
     async recipeSearchCategory() {
       //load function asynchronically
       let response;
-      let apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + this.userInput;
+      let apiUrl =
+        "https://www.themealdb.com/api/json/v1/1/filter.php?c=" +
+        this.userInput;
       try {
         response = await this.axios
-            .get(apiUrl) //promise should await this response
-            .then((response) => (this.mealInfo = response.data.meals));
+          .get(apiUrl) //promise should await this response
+          .then((response) => (this.mealInfo = response.data.meals));
         console.log(response);
       } catch (e) {
         console.error(e);
@@ -84,11 +110,13 @@ export default {
     async recipeSearchArea() {
       //load function asynchronically
       let response;
-      let apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + this.userInput;
+      let apiUrl =
+        "https://www.themealdb.com/api/json/v1/1/filter.php?a=" +
+        this.userInput;
       try {
         response = await this.axios
-            .get(apiUrl) //promise should await this response
-            .then((response) => (this.mealInfo = response.data.meals));
+          .get(apiUrl) //promise should await this response
+          .then((response) => (this.mealInfo = response.data.meals));
         console.log(response);
       } catch (e) {
         console.error(e);
